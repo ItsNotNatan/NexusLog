95358f2dd538c7703e27febecc0cdd7e248da1d7
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-26 11:25:04 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+
+// A correção está aqui: usamos '../' para voltar uma pasta para trás
 import Sidebar from './components/Sidebar/Sidebar';
 
-// 1. Recebemos o "modulo" aqui em cima
 export default function AppLayout({ modulo }) {
   return (
     <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: '#f4f5f7' }}>
       
-      {/* 2. Passamos o "modulo" para a Sidebar saber qual menu desenhar! */}
+      {/* Passamos o "modulo" para a Sidebar saber qual menu desenhar! */}
       <Sidebar modulo={modulo} />
       
       <main style={{ flex: 1, overflowX: 'hidden' }}>
