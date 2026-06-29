f4c96c76563f29c956103202b77e575b8a38a428
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-29 09:48:46 -0300

    Add files via upload

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 
-export default function AppLayout() {
+// Aqui recebemos a propriedade "modulo" (que vem do routes.jsx)
+export default function AppLayout({ modulo }) {
   return (
     <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: '#f4f5f7' }}>
       
-      {/* Esquerda: Barra de Navegação */}
-      <Sidebar />
+      {/* Passamos o "modulo" para a Sidebar saber qual menu desenhar! */}
+      <Sidebar modulo={modulo} />
       
       {/* Direita: O conteúdo dinâmico (As tuas páginas) */}
       <main style={{ flex: 1, overflowX: 'hidden' }}>
