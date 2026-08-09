5b6a196740cff82ffff23851caab55bb8043ef7e
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-08-09 20:41:05 -0300

    Delete src directory

--- src/routes/estoqueRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/estoqueRoutes.js
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/estoqueController');
-
-// 🟢 ROTA PÚBLICA: O cliente pode ver o estoque sem login
-router.get('/listar', ctrl.listar);
-
-module.exports = router;
